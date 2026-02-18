6edd162c709f57f43f98a3887ba01a83fd007b3c
Laura Basilio Mitterbach <laurabasiliomitter@gmail.com>
2026-02-18 08:54:27 -0300

    ABCGJL-29 chore: Criar projeto Spring Boot com Java 21

---
+++ src/main/java/com/senai/abcgjl_smartcusine_backend/AbcgjlSmartcusineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.senai.abcgjl_smartcusine_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcgjlSmartcusineBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcgjlSmartcusineBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senai/abcgjl_smartcusine_backend/AbcgjlSmartcusineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.abcgjl_smartcusine_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcgjlSmartcusineBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
