f1bbac2405918bce368b7a0e0c144d4bb3eb6372
Bianca Neves <bianevestorres08@gmail.com>
2026-02-25 09:12:59 -0300

    git commit -m "ABCGJL-35 Definir endpoints por recurso (ex: /usuarios, /auth/login) "

---
+++ src/main/java/com/senai/abcgjl_smartcusine_backend/application/service/AuthService.java
@@ -0,0 +1,28 @@
+package com.senai.abcgjl_smartcusine_backend.application.service;
+
+import com.senai.abcgjl_smartcusine_backend.application.dto.AuthDTO;
+import com.senai.abcgjl_smartcusine_backend.domain.entity.UsuarioEntity;
+import com.senai.abcgjl_smartcusine_backend.domain.exception.UsuarioNaoEncontradoException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UsuarioRepository usuarios;
+    private final PasswordEncoder encoder;
+    private final JwtService jwt;
+
+    public String login(AuthDTO.LoginRequest req) {
+        UsuarioEntity usuario = usuarios.findByEmail(req.email())
+                .orElseThrow(() ->  new UsuarioNaoEncontradoException("Usuário não encontrado"));
+
+        if (!encoder.matches(req.senha(), usuario.getSenha())) {
+            throw new BadCredentialsException("Credenciais inválidas");
+        }
+
+        return jwt.generateToken(usuario.getEmail(), usuario.getRole().name());
+    }
+}
