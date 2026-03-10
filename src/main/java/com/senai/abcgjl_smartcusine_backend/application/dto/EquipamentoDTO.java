24c30c06a159e6b95cc59e5eac813a9868a481d2
Gabrieli da Silva Marcelino <gabih.12161@gmail.com>
2026-03-10 09:29:47 -0300

    git commit -m "ABCGJL-70 feat: criação do CRUD e EquipamentoDTO para entidade Equipamento"

--- src/main/java/com/senai/abcgjl_smartcusine_backend/application/dto/EquipamentoDTO.java
+++ src/main/java/com/senai/abcgjl_smartcusine_backend/application/dto/EquipamentoDTO.java
@@ -1,4 +1,24 @@
 package com.senai.abcgjl_smartcusine_backend.application.dto;
 
-public class EquipamentoDTO {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
+public record EquipamentoDTO (
+
+
+    UUID idEquipamento,
+
+    @NotBlank(message = "O tipo do equipamento não pode estar vazio")
+    String tipo,
+
+    @NotNull(message = "A temperatura atual não pode ser nula")
+    Double temperaturaAtual,
+
+    @NotNull(message = "A temperatura ideal não pode ser nula")
+    Double temperaturaIdeal
+
+) {
+
 }
