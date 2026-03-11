114cc76bf70c36a580147c45360910367abf8e05
Laura Basilio Mitterbach <laurabasiliomitter@gmail.com>
2026-03-11 08:56:03 -0300

    ABCGJL-71 fix: Implementar cadastro de usuário (por tipo quando aplicável)

--- src/main/java/com/senai/abcgjl_smartcusine_backend/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/senai/abcgjl_smartcusine_backend/infrastructure/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.senai.abcgjl_smartcusine_backend.infrastructure.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,5 +21,10 @@ public class SecurityConfig {
                 .formLogin(login -> login.disable()); // desativa login de formulário
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
