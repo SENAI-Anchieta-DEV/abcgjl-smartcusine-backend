6cec58aee2c6291deae2010add7ed9ce0ccb129a
Gabrieli da Silva Marcelino <gabih.12161@gmail.com>
2026-03-17 10:52:50 -0300

    git commit -m "ABCGJL-59, ABCGJL-60, ABCGJL-61, ABCGJL-62, ABCGJL-63, ABCGJL-64, feat: Configurar Spring Security + JWT (filtros e regras base) | Implementar filtro/validação do JWT | Definir endpoints públicos e protegidos"

--- src/main/java/com/senai/abcgjl_smartcusine_backend/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/senai/abcgjl_smartcusine_backend/infrastructure/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,8 +23,27 @@ public class SecurityConfig {
 
         http
                 .csrf(csrf -> csrf.disable())
+
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**", "/usuarios").permitAll()
+                        .requestMatchers("/auth/**", "/usuarios", "/v3/api-docs/**", "/swagger-ui/**" //endpoints públicos
+                        ).permitAll()
+
+                        //EndPoints das entidades protegidos
+                        .requestMatchers(
+                                "/alertas/**",
+                                "/equipamentos/**",
+                                "/fichastecnicas/**",
+                                "/fichastecnicalinsumos/**",
+                                "/insumos/**",
+                                "/relatorios/**",
+                                "/temporizadores/**",
+                                "/usuarios/**"
+                        ).authenticated()
+
+                        //EndPoints das entidades que não foram mencionadas caso tenha
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
