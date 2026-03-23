2e928ca90a753c3400d2271d06da6789767143b2
Laura Basilio Mitterbach <laurabasiliomitter@gmail.com>
2026-03-23 09:56:19 -0300

    ABCGJL-69 fix: Garantir que acesso indevido gere 403 padronizado

--- src/main/java/com/senai/abcgjl_smartcusine_backend/infrastructure/security/CustomAccessDeniedHandler.java
+++ src/main/java/com/senai/abcgjl_smartcusine_backend/infrastructure/security/CustomAccessDeniedHandler.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 
 @Component
 
-public class CostomAccessDeniedHandler implements AccessDeniedHandler {
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
 
     @Override
