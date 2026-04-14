62e01c201b5031207171df25ff0e19540a908bbc
Laura Basilio Mitterbach <laurabasiliomitter@gmail.com>
2026-04-14 10:14:47 -0300

    render

--- src/main/java/com/senai/abcgjl_smartcusine_backend/AbcgjlSmartcusineBackendApplication.java
+++ src/main/java/com/senai/abcgjl_smartcusine_backend/AbcgjlSmartcusineBackendApplication.java
@@ -1,14 +1,16 @@
-/*package com.senai.abcgjl_smartcusine_backend;
+package com.senai.abcgjl_smartcusine_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class AbcgjlSmartcusineBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AbcgjlSmartcusineBackendApplication.class, args);
 	}
 
-}*/
+}
 
