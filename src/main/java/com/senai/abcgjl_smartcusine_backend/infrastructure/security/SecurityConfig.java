82ad96ddd69cc38d40cd2bd004e7fe271eb69a88
Laura Basilio Mitterbach <laurabasiliomitter@gmail.com>
2026-04-14 10:25:44 -0300

    render

--- src/main/java/com/senai/abcgjl_smartcusine_backend/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/senai/abcgjl_smartcusine_backend/infrastructure/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.senai.abcgjl_smartcusine_backend.infrastructure.security;
 
+import com.senai.abcgjl_smartcusine_backend.infrastructure.config.CorsProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,11 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableMethodSecurity
